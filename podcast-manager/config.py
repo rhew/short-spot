d9d4ecfac2d9fe7af57d5755fa7bf22e5fb9eb46
James Rhew <james@rhew.org>
2024-11-13 11:48:06 -0500

    Update since

--- podcast-manager/config.py
+++ podcast-manager/config.py
@@ -2,22 +2,22 @@ feeds = [
     {
         'name': '99pi',
         'url': 'https://feeds.99percentinvisible.org/99percentinvisible',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     {
         'name': 'AstronomyCast',
         'url': 'https://astronomycast.libsyn.com/rss',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     {
         'name': 'Freakonomics',
         'url': 'https://feeds.simplecast.com/Y8lFbOT4',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     {
         'name': 'GastroPod',
         'url': 'https://feeds.megaphone.fm/VMP6255701211',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     # {
     #     'name': 'HardcoreHistory',
@@ -27,22 +27,22 @@ feeds = [
     {
         'name': 'RadioLab',
         'url': 'https://www.wnycstudios.org/feeds/series/podcasts?limit=10',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     {
         'name': 'TEDRadioHour',
         'url': 'https://feeds.npr.org/510298/podcast.xml',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     {
         'name': 'TheRecipe',
         'url': 'https://feed.therecipepodcast.com/',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     {
         'name': 'ThisAmericanLife',
         'url': 'https://www.thisamericanlife.org/podcast/rss.xml',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
     # {
     #     'name': 'UpFirst',
@@ -52,6 +52,6 @@ feeds = [
     {
         'name': 'WWDTM',
         'url': 'https://feeds.npr.org/344098539/podcast.xml',
-        'since': '2024-10-18'
+        'since': '2024-10-19'
     },
 ]
